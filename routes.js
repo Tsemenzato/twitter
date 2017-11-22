0e34f3a937590bc83146d40023ee0bc8763426b4
tsemenzato <tomas.semenzato@nan-labs.com>
2017-11-22 15:01:45 -0300

    Added tweet creation

--- routes.js
+++ routes.js
@@ -1,8 +1,12 @@
 const UserController = require('./src/user/UserController');
 const userController = new UserController();
-var express = require('express');
 
+const TweetController = require('./src/tweet/TweetController');
+const tweetController = new TweetController();
+
+var express = require('express');
 var router = express.Router();
+
 
 router.get('/users', userController.getAll);
 
@@ -13,5 +17,7 @@ router.delete('/users/:id', userController.delete);
 router.post('/users', userController.post);
 
 router.put('/users', userController.put);
+
+router.get('/users/:user/tweets',tweetController.get);
 
 module.exports = router;
