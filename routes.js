ee6e611337dc096f51cc10bcd3bf5e7af34ab28e
tsemenzato <tomas.semenzato@nan-labs.com>
2017-12-22 17:33:13 -0300

    Add feed functionality

--- routes.js
+++ routes.js
@@ -18,6 +18,26 @@ const dbd = require('./devtools/database-interfacing/dbDump');
 var express = require('express');
 var router = express.Router();
 
+router.use(function (req, res, next) {
+    
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:8080');
+        res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:8081');
+        
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+    
+        // Pass to next layer of middleware
+        next();
+    });
+
 
 router.get('/users', userController.getAll);
 
@@ -56,7 +76,7 @@ router.get('/users/:user/feed', feedController.get)
 
 
 
-router.get('/', dbd)
+router.get('/dbd', dbd)
 
 
 module.exports = router;
